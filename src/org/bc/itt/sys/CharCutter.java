69ec7c887a9abe8707c5397a011017c668ea4b7d
runningship <runningship@gmail.com>
2016-01-17 21:16:40 +0800

    more

--- src/org/bc/itt/sys/CharCutter.java
+++ src/org/bc/itt/sys/CharCutter.java
@@ -38,7 +38,50 @@ public class CharCutter {
 		return lines;
 	}
 	
+	public static List<BufferedImage> processToChar(BufferedImage lineData){
+		int left=0;
+		boolean columnStart=false;
+		List<BufferedImage> chars = new ArrayList<BufferedImage>();
+		for(int i=0;i<lineData.getWidth();i++){
+			boolean xIsEmpty = true;
+			for(int j=0;j<lineData.getHeight();j++){
+				int color = lineData.getRGB(i, j);
+				if(color==-16777216){
+					//black
+					xIsEmpty = false;
+					break;
+				}
+			}
+			if(xIsEmpty==false && columnStart==false){
+				columnStart = true;
+				left = i;
+			}else if(xIsEmpty==true && columnStart==true){
+				//column finish
+				columnStart = false;
+				BufferedImage charc = lineData.getSubimage(left, 0, i-left, lineData.getHeight());
+				chars.add(charc);
+			}
+		}
+		return chars;
+	}
+	
+	
 	public static void main(String[] args) throws IOException{
+		//testLines();
+		testChars();
+	}
+	
+	private static void testChars() throws IOException{
+		String dir = "E:\\java\\xinzhouy\\TextRecognizer\\text reco\\";
+		String fileName = "p1.jpg.r.jpg.9.jpg";
+		BufferedImage img = javax.imageio.ImageIO.read(new File(dir+fileName));
+		List<BufferedImage> lines = processToChar(img);
+		for(int i=0;i<lines.size();i++){
+			ImageIO.write(lines.get(i), "png", new File(dir+fileName+"."+i+".jpg"));
+		}
+	}
+	
+	private static void testLines() throws IOException{
 		String dir = "C:\\Users\\Administrator\\Desktop\\text reco\\";
 		String fileName = "p1.jpg.r.jpg";
 		BufferedImage img = javax.imageio.ImageIO.read(new File(dir+fileName));
